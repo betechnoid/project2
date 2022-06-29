c280041971c6ee62fc7fb9bd1654ca4ab2990682
aditisah <sditi.sah1@gmail.com>
2022-06-29 11:59:28 +0530

    getApi code

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -3,14 +3,33 @@ const collegeModel = require("../models/collegeModel")
 const createCollege = async function(req,res){
     try{
         const collegeDetails = req.body;
-        const newCollege = collegeModel.create(collegeDetails);
-        res.status(201).send({status: true, data: newCollege})
+        const newCollege = await collegeModel.create(collegeDetails);
+        res.status(201).send({status: true, data: {newCollege}})
     }
 catch(err){
     res.status(500).send({status: false, msg: err.message})
 }
 }
 
+const getCollegeWithInterns = async function(req,res){
+    try{
+        const collegeAbrv = req.query;
+ const college = await collegeModel.findOne(collegeAbrv);
+ console.log(college)
+ if(!college){
+    return res.status(404).send({status: false, msg: "College not found"})
+ }
+ const collegeWithInterns = await internModel.find({collegeId: college._id})
+ if(collegeWithInterns !== 0){
+    return res.status(404).send({status: false, msg: "interns with given college id not found"})
+ }
+ res.status(200).send({status: true, data: collegeWithInterns})
+    }
 
+    catch(err){
+       return res.status(500).send({status: false, msg: err.message})
+    }
+}
 
 module.exports.createCollege = createCollege
+module.exports.getCollegeWithInterns = getCollegeWithInterns
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,6 +5,7 @@ const collegeController = require("../controller/collegeController");
 
 
 router.post("/functionup/colleges",collegeController.createCollege)
+router.get("/functionup/collegeDetails",collegeController.getCollegeWithInterns)
 
 
 module.exports = router;
