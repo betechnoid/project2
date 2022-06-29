b2c97b5cf016d7e27ece08b215a252a96a3dcba1
aditisah <sditi.sah1@gmail.com>
2022-06-29 15:12:17 +0530

    get api done

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,5 +1,5 @@
 const collegeModel = require("../models/collegeModel")
-
+const internModel = require("../models/internModel")
 const createCollege = async function(req,res){
     try{
         const collegeDetails = req.body;
@@ -14,17 +14,24 @@ catch(err){
 const getCollegeWithInterns = async function(req,res){
     try{
         const collegeAbrv = req.query;
- const college = await collegeModel.findOne(collegeAbrv);
+       if(Object.keys(collegeAbrv).length===0 || !collegeAbrv.name){
+        return res.status(400).send({status: false, msg: 'Please select name'})
+       } 
+        const validCollegeAbbrviation = collegeAbrv["name"].trim();
+        console.log(validCollegeAbbrviation)
+ const college = await collegeModel.findOne({name: validCollegeAbbrviation}).select({name: 1, fullName: 1,logoLink: 1});
  console.log(college)
  if(!college){
     return res.status(404).send({status: false, msg: "College not found"})
  }
- const collegeWithInterns = await internModel.find({collegeId: college._id})
- if(collegeWithInterns !== 0){
+ const interns = await internModel.find({collegeId: college._id}).select({_id: 1,name: 1,email: 1,mobile: 1})
+ console.log(interns)
+ if(interns.length === 0){
     return res.status(404).send({status: false, msg: "interns with given college id not found"})
  }
- res.status(200).send({status: true, data: collegeWithInterns})
-    }
+ let collegeDetail = {name: college.name, fullName: college.fullName,logoLink: college.logoLink, interns: interns }
+   return res.status(200).send({status: true, data: collegeDetail})
+     }
 
     catch(err){
        return res.status(500).send({status: false, msg: err.message})
